0a3058954460a37448bf9563ea5c1e64a2e4b985
JkeyLove <1826067520@qq.com>
2023-06-17 16:11:19 +0800

    Gateway路由过滤器

--- user-service/src/main/java/org/example/controller/UserController.java
+++ user-service/src/main/java/org/example/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
-        System.out.println("我被调用了");
+        System.out.println("user被调用了");
         return service.getUserById(uid);
     }
 }
